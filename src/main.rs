1e3248af18c81baaa072e3757082b6e626f2748c
Ash <ValleyKnight@protonmail.com>
2020-03-12 18:16:51 -0400

    add ability to list packages

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate clap;
+extern crate glob;
 use clap::{Arg, App};
+use glob::glob;
 use std::process::Command;
 
 fn main() {
@@ -13,7 +15,19 @@ fn main() {
 			 .help("Search for a package.")
 		     .value_name("SEARCH")
 		     .takes_value(true))
+		.arg(Arg::with_name("list")
+			 .short("l")
+			 .long("list")
+			 .help("List currently installed packages."))
 		.get_matches();
+	if matches.is_present("list") {
+		for entry in glob("/var/db/pkg/*/*/").expect("Failed to read glob pattern") {
+            match entry {
+                Ok(path) => println!("{:?}", path.display()),
+                Err(e) => println!("{:?}", e),
+            }
+        }
+	}
 	if let Some(s) = matches.value_of("search") {
         let results = Command::new("emerge")
 			.arg("-s")
